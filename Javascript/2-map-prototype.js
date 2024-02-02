6e427d04dea12e3e969818e8157c8d1ade6bd7ca
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2024-02-02 16:48:14 +0530

    Update 2-map-prototype.js

--- Javascript/2-map-prototype.js
+++ Javascript/2-map-prototype.js
@@ -1,1 +1,19 @@
+Array.prototype.myMap = function(fn) {
+  let arr = this;
+  for(let i=0;i<arr.length;i++){
+    arr[i] = fn(arr.slice(0,i+1));
+  }
+  return arr;
+};
 
+
+let arr = [1,2,3,4];
+function add(arr){
+  let sum = 0;
+  for(let i=0;i<arr.length;i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+const res = arr.myMap(add);
+console.log(res);
