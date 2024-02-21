c23eb6dd0c5913288e93be3b32b2bd346dfa9476
vikas.t <vikas.t@pepperfry.com>
2024-02-21 08:56:53 +0530

    range slider

--- CSS/3-range-slider/script.js
+++ CSS/3-range-slider/script.js
@@ -1,8 +1,11 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("sliderValue");
+const circle = document.getElementById("circle");
 output.innerHTML = slider.value;
 
 slider.oninput = function(event) {
     console.log(event);
     output.innerHTML = this.value;
+    circle.style.width = this.value + "px";
+    circle.style.height = this.value + "px";
 }
