97e119377f692cfc4d2fb4f2286aba2931ddc857
vikas.t <vikas.t@pepperfry.com>
2024-02-22 09:38:11 +0530

    search throttle has been added

--- React/watchlist/src/components/WatchList.js
+++ React/watchlist/src/components/WatchList.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Watchlist = ({stocks}) => {
   const [watchlist, setWatchlist] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [stocksData, setStocksData] = useState(stocks);
+  const [filteredWatchlist, setFilteredWatchlist] = useState([]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const filteredList = watchlist.filter(stock =>
+        stock.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      console.log('called');
+      setFilteredWatchlist(filteredList);
+    }, 500);
+
+    // Cleanup function to clear the timeout if searchQuery changes before the timeout is reached
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery, watchlist]);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
@@ -17,10 +30,6 @@ const Watchlist = ({stocks}) => {
     }
   };
 
-  const filteredWatchlist = watchlist.filter((stock) =>
-    stock.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
   return (
     <div>
       <h2>Stock Watchlist</h2>
@@ -31,7 +40,7 @@ const Watchlist = ({stocks}) => {
         onChange={handleSearch}
       />
 
-      {stocksData.map((stock) => (
+      {stocks.map((stock) => (
         <button key={stock.id} onClick={() => handleToggleWatchlist(stock.name)}>
             {watchlist.includes(stock.name) ? 'Remove '+stock.name : 'Add '+stock.name}
         </button>
