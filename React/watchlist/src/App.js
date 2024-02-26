04f36ff8c996873d071f6c55f6b10bc266cd543a
vikas.t <vikas.t@pepperfry.com>
2024-02-26 18:53:27 +0530

    appointment list

--- React/watchlist/src/App.js
+++ React/watchlist/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-import Watchlist from './components/WatchList';
-import stocks from './data/stocksData';
+import React, { useState } from "react";
+import "./App.css";
+import Watchlist from "./components/WatchList";
+import stocks from "./data/stocksData";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(3);
+
+    setCount(count + 2);
+
+    setCount((prev) => prev + 2);
+
+    setCount(count - 4);
+
+    setCount((prev) => prev + 6);
+  };
+
   return (
     <div className="App">
       <Watchlist stocks={stocks} />
+      <div onClick={handleClick}>Interview Question -- {count}</div>
     </div>
   );
 }
