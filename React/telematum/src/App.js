aeb50ffa272ffc941a7cd01ea028fbf5186b3f90
vikas tiwari <tiwarivikkas3001@gmail.com>
2024-02-27 22:29:28 +0530

    telematum project

---
+++ React/telematum/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Table from './components/Table/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+    </div>
+  );
+}
+
+export default App;
