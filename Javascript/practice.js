e9f22416c9134749d356efec91da6ff290ddd8b9
tiwarivikkas <51874681+vikkastiwari@users.noreply.github.com>
2024-02-28 21:57:38 +0530

    bubble sort

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -2,10 +2,10 @@ function flattenObject(obj, parentKey = '', ) {
     let result = {};
     for(const key in obj){
         if(obj.hasOwnProperty(key)){
-            let newKey = parentKey ? `${parentKey}.${key}` : key;
-            if(typeof obj[key] === 'object' && obj[key]!==null){
-                let res = flattenObject(obj[key],newKey);
-                result = {...result,...res};
+            const newKey = parentKey ? `${parentKey}.${key}` : key;
+            if(typeof obj[key] === 'object' && obj[key] !== null){
+                let res = flattenObject(obj[key], newKey);
+                result = {...result, ...res};
             }else{
                 result[newKey] = obj[key];
             }
@@ -26,3 +26,25 @@ const nestedObject = {
 
 const flattenedObject = flattenObject(nestedObject);
 console.log(flattenedObject);
+
+Promise.myRace = function(promises) {
+    return new Promise((resolve,reject) => {
+        promises.forEach(promise => {
+            Promise.resolve(promise).then(resolve,reject);
+        });
+    })
+}
+
+const myPromise1 = new Promise((resolve) => {
+    setTimeout(() => resolve('Success! 1'), 3000);
+});
+
+const myPromise2 = new Promise((resolve) => {
+    setTimeout(() => resolve('Success! 2'), 1000);
+});
+
+const res = Promise.myRace([myPromise1,myPromise2]).then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
